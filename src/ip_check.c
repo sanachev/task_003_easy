8be34a8f122cb4c522cf78097332fc1f8243cff1
harrioho <harrioho@example.com>
2024-03-05 00:27:15 +0300

    develop some code again

--- src/ip_check.c
+++ src/ip_check.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include "ip_check.h"
+
+#include <stdio.h>
 #define LENGTH 16
 
 /* Практика приводит к совершенству.
 Хороший код говорит сам за себя. */
 
-int main(){
+int main() {
     char string[LENGTH];
-
 
     return 0;
 }
--- src/ip_check.h
+++ src/ip_check.h
@@ -1,6 +1,6 @@
 #ifndef IP_CHECK_H
 #define IP_CHECK_H
-void input(*string);
+void input(char *string);
 void check();
 void output();
 #endif
