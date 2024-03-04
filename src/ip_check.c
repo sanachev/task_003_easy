45879ee549976e76fe7d12947be535b2debf3c45
harrioho <harrioho@example.com>
2024-03-04 23:33:39 +0300

    develop some code

---
+++ src/ip_check.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "ip_check.h"
+#define LENGTH 16
+
+/* Практика приводит к совершенству.
+Хороший код говорит сам за себя. */
+
+int main(){
+    char string[LENGTH];
+
+
+    return 0;
+}
---
+++ src/ip_check.h
@@ -0,0 +1,6 @@
+#ifndef IP_CHECK_H
+#define IP_CHECK_H
+void input(*string);
+void check();
+void output();
+#endif
