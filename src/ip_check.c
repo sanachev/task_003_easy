72f437a4b0f52f4ab3158a471c69e7c5a65a751d
harrioho <harrioho@example.com>
2024-03-05 21:59:14 +0300

    develop add arpa/inet

--- src/ip_check.c
+++ src/ip_check.c
@@ -1,13 +1,20 @@
+#include <stdio.h>
+#include <arpa/inet.h>
 #include "ip_check.h"
 
-#include <stdio.h>
+
 #define LENGTH 16
 
 /* Практика приводит к совершенству.
 Хороший код говорит сам за себя. */
 
 int main() {
-    char string[LENGTH];
+    char address[LENGTH];
+    int check;
+    input(address);
+    check = inet_aton(const address, struct in_addr *addrptr);
+    output(check);
+    
 
     return 0;
 }
